e06e76e79140229409b234f029890b5c24c41377
25479ns <25479ns@hvhs.school.nz>
2026-06-07 08:16:29 +0000

    Jun 7, 2026, 8:16 AM

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("Hello world!")
+
+<script>
+function addToCart(name, price, image, qtyId) {
+
+    let quantity = document.getElementById(qtyId).value;
+
+    if(quantity <= 0 || quantity == ""){
+        alert("Enter quantity");
+        return;
+    }
+
+    let total = price * quantity;
+
+    localStorage.setItem("itemName", name);
+    localStorage.setItem("itemPrice", price);
+    localStorage.setItem("itemQty", quantity);
+    localStorage.setItem("itemImage", image);
+    localStorage.setItem("itemTotal", total);
+
+    window.location.href = "cart.html";
+}
+</script>
