be952695317b163c37dcc6e19b001b936a92d182
25479ns <25479ns@hvhs.school.nz>
2026-06-08 06:53:24 +0000

    Jun 8, 2026, 6:53 AM

--- script.js
+++ script.js
@@ -24,9 +24,3 @@ function Add_To_Cart_Button(id, name, price, image, quantityInputId)
 
     alert("Item added to cart!");
 }
-
-function ResetCart()
-{
-    localStorage.removeItem("cart")
-    location.reload();
-}
