abd9d084cb88111a4fb8d451f375159ec9237beb
25479ns <25479ns@hvhs.school.nz>
2026-06-08 22:56:47 +0000

    Jun 8, 2026, 10:56 PM

--- JS/cart.js
+++ JS/cart.js
@@ -44,10 +44,10 @@ function ResetCart() {
 
 function SubmitInfo() {
 
-if ("customerMoney" >= "grandTotal") {
-    alert("Your order has been placed")
+if ("customerMoney >= grandTotal") {
+    alert ("Your order has been placed");
 } else {
-    alert("You have infficient funds. Please Try Again")
+    alert ("You have infficient funds. Please Try Again");
 }
 
 }
