4333bde4fc93ab90e9f12622757844279d828b4c
25479ns <25479ns@hvhs.school.nz>
2026-06-15 22:14:50 +0000

    Jun 15, 2026, 10:14 PM

---
+++ JS/Search.js
@@ -0,0 +1,8 @@
+function filterContent() {
+    const searchInput = document.getElementById('searchBar').value.toLowerCase();
+    const items = document.querySelectorAll('.searchable-item');
+    items.forEach(item => {
+        const textContent = item.textContent.toLowerCase();
+        if
+    }
+}
